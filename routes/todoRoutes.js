e9add07a731ad744e1b0353ffd3bf881fa31a70a
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-01-09 21:02:05 -0800

    added delete route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -11,4 +11,9 @@ module.exports = app => {
     .then(items => res.json(items))
     .catch(e => console.error(e))
   })
+  app.delete('/items/:id', (req, res) => {
+    ToDo.deleteOne( { _id: req.params.id } ) // Get the row of the task of the ID.
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
 }
